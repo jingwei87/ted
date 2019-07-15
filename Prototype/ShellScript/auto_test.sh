c7037592ee745b344cb666c2940932ad891bdd73
tinoryj <tinoryj@gmail.com>
2019-07-15 16:45:03 +0800

    prototype ready

--- Prototype/ShellScript/auto_test.sh
+++ Prototype/ShellScript/auto_test.sh
@@ -3,12 +3,12 @@ kill -9 $(ps -ef|grep SERVER|grep -v grep|awk '{print $2}')
 kill -9 $(ps -ef|grep KEYMANAGER|grep -v grep|awk '{print $2}')
 ./auto_config.sh
 cd ../server
-./SERVER 11030 11034&
+./SERVER 11030&
 cd ../server2
-./SERVER 11031 11035&
+./SERVER 11031&
 cd ../server3
-./SERVER 11032 11036&
+./SERVER 11032&
 cd ../server4
-./SERVER 11033 11037&
+./SERVER 11033&
 cd ../keyServer
 ./KEYMANAGER 19301&
