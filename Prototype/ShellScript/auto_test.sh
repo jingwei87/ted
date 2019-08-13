fa95465df8c28989f7ff0d5f94f51647bfe0c20b
tinoryj <tinoryj@gmail.com>
2019-08-13 15:32:32 +0800

    Update keys & configure for old version not functional

--- Prototype/ShellScript/auto_test.sh
+++ Prototype/ShellScript/auto_test.sh
@@ -3,12 +3,12 @@ kill -9 $(ps -ef|grep SERVER|grep -v grep|awk '{print $2}')
 kill -9 $(ps -ef|grep KEYMANAGER|grep -v grep|awk '{print $2}')
 ./auto_config.sh
 cd ../server
-./SERVER 11030&
+./SERVER 1130&
 cd ../server2
-./SERVER 11031&
+./SERVER 1131&
 cd ../server3
-./SERVER 11032&
+./SERVER 1132&
 cd ../server4
-./SERVER 11033&
+./SERVER 1133&
 cd ../keyServer
 ./KEYMANAGER 19301&
